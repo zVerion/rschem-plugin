311c8faf8b9196e7901d971b5d2af09caf71b910
zVerion <zverion.geschaeft@gmail.com>
2026-03-16 20:08:20 +0100

    docs(command-description): add @since tag and fix missing period in @return tag

--- command/src/main/java/me/verion/rschem/command/annotation/CommandDescription.java
+++ command/src/main/java/me/verion/rschem/command/annotation/CommandDescription.java
@@ -1,4 +1,4 @@
-package me.verion.rschem.internal.command.annotation;
+package me.verion.rschem.command.annotation;
 
 import lombok.NonNull;
 
@@ -7,6 +7,8 @@ import java.lang.annotation.*;
 /**
  * Attaches a human-readable description to a {@link SubCommand}-annotated method.
  * Descriptions are surfaced in the auto-generated {@code /cmd help} output.
+ *
+ * @since 2.0
  */
 @Documented
 @Target(ElementType.METHOD)
@@ -16,7 +18,7 @@ public @interface CommandDescription {
   /**
    * A short, single-line description of what this sub-command does.
    *
-   * @return the non-empty description string
+   * @return the non-empty description string.
    */
   @NonNull String value();
 }
